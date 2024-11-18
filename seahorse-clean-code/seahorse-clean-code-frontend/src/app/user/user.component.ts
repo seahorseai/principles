87f8c4137155116bcd5766ed8584486ba0db9d83
Pedro5657 <pedrogarciasantana04@gmail.com>
2024-11-18 08:27:43 +0000

    Seahorse Clean Code done

---
+++ seahorse-clean-code/seahorse-clean-code-frontend/src/app/user/user.component.ts
@@ -0,0 +1,23 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: 'app-user',
+  template: `{{ user.name }}`,
+})
+export class UserComponent {
+  user: User;
+
+  constructor() {
+    this.user = this.getUser();
+  }
+
+  private getUser(): User {
+    return { name: 'John Doe', age: 30, role: 'Admin' }; // Mock data
+  }
+}
+
+interface User {
+  name: string;
+  age: number;
+  role: string;
+}
