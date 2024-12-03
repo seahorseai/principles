9e1c74167544dacfb7948cba8e1d3d03af826a86
Pedro5657 <pedrogarciasantana04@gmail.com>
2024-12-03 15:06:03 +0000

    Solid's DIP code done

--- solid/5.D/index.ts
+++ solid/5.D/index.ts
@@ -1,7 +1,7 @@
-import { EmailNotification } from "./classes/EmailNotificacion.class";
-import { NotificationService } from "./classes/NotificationService.class";
-import { PushNotification } from "./classes/PushNotifications.class";
-import { TextMessageNotification } from "./classes/TextMessageNotification";
+import { EmailNotification } from './classes/EmailNotification.class';
+import { TextMessageNotification } from './classes/TextMessageNotification.class';
+import { PushNotification } from './classes/PushNotification.class';
+import { NotificationService } from './classes/NotificationService.class';
 
 const emailNotifier = new EmailNotification();
 const textMessageNotifier = new TextMessageNotification();
@@ -12,4 +12,3 @@ const pushService = new NotificationService(pushNotifier);
 emailService.notify("Hello, this is an email notification!");
 textMessageService.notify("Hello, this is a text message notification!");
 pushService.notify("Hello, this is a push notification!");
-
